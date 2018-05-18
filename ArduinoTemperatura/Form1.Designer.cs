c6850311d10aa9c385bb78e2d8b1a91d999dbe8b
faraday58 <jarodriguezvera@gmail.com>
2018-05-18 11:20:51 -0500

    Configurando Puerto

--- ArduinoTemperatura/Form1.Designer.cs
+++ ArduinoTemperatura/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnEncender = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEncender
+            // 
+            this.btnEncender.Location = new System.Drawing.Point(73, 38);
+            this.btnEncender.Name = "btnEncender";
+            this.btnEncender.Size = new System.Drawing.Size(75, 23);
+            this.btnEncender.TabIndex = 0;
+            this.btnEncender.Text = "Encender";
+            this.btnEncender.UseVisualStyleBackColor = true;
+            this.btnEncender.Click += new System.EventHandler(this.btnEncender_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.Location = new System.Drawing.Point(206, 38);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(75, 23);
+            this.btnApagar.TabIndex = 0;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = true;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(461, 246);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnEncender);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEncender;
+        private System.Windows.Forms.Button btnApagar;
     }
 }
 
--- ArduinoTemperatura/Form1.cs
+++ ArduinoTemperatura/Form1.cs
@@ -1,20 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO.Ports;
 namespace ArduinoTemperatura
 {
     public partial class Form1 : Form
     {
+        SerialPort serialPort;
         public Form1()
         {
             InitializeComponent();
+            serialPort = new SerialPort();
+            serialPort.BaudRate=9600;
+            serialPort.PortName = "COM6";
+            serialPort.Open();
+        }
+
+        private void btnEncender_Click(object sender, EventArgs e)
+        {
+            serialPort.Write("a");
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            serialPort.Write("b");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            serialPort.Close();
         }
     }
 }
